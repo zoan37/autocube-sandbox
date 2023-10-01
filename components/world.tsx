b224c8d093114194f8c39277d8047a9b4c572bb8
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-10-01 16:02:47 -0400

    animation

--- components/world.tsx
+++ components/world.tsx
@@ -5,6 +5,7 @@ import { VRMLoaderPlugin, VRMUtils } from '@pixiv/three-vrm';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
+import { loadMixamoAnimation } from './loadMixamoAnimation.js';
 
 const Scene = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -12,7 +13,9 @@ const Scene = () => {
 
   function init() {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true
+    });
     rendererRef.current = renderer;
 
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -21,16 +24,32 @@ const Scene = () => {
     const pmremGenerator = new THREE.PMREMGenerator(renderer);
 
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x0);
+    // alice blue color
+    // scene.background = new THREE.Color(0xF0F8FF);
+    scene.background = new THREE.Color(0xe0e0e0);
+    scene.fog = new THREE.Fog(0xe0e0e0, 20, 100);
+
     scene.environment = pmremGenerator.fromScene(new RoomEnvironment(renderer), 0.04).texture;
 
     // Add ambient light
+    /*
     var ambientLight = new THREE.AmbientLight(0x404040); // soft white light
     scene.add(ambientLight);
 
     const light = new THREE.DirectionalLight(0xffffff);
     light.position.set(1.0, 1.0, 1.0).normalize();
     scene.add(light);
+    */
+
+    // lights
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 1);
+    hemiLight.position.set(0, 20, 0);
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(0, 20, 10);
+    scene.add(dirLight);
 
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -38,35 +57,67 @@ const Scene = () => {
     // scene.add(cube);
 
     // Adding a grid to the scene
-    const gridHelper = new THREE.GridHelper(10, 10);
-    scene.add(gridHelper);
+    // const gridColor = 0xD3D3D3;
+    // const gridHelper = new THREE.GridHelper(10, 10, gridColor, gridColor);
+    // scene.add(gridHelper);
+
+    const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false }));
+    mesh.rotation.x = - Math.PI / 2;
+    scene.add(mesh);
+
+    const grid = new THREE.GridHelper(200, 200, 0x000000, 0x000000);
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    scene.add(grid);
 
     // Adding orbit controls
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.dampingFactor = 0.25;
     controls.enableZoom = true;
+    controls.minDistance = 1;
+    controls.maxDistance = 50;
 
     camera.position.z = 5;
     camera.position.y = 2;
 
+    const vrmList = [] as any[];
+    const mixerList = [] as any[];
+
+    const clock = new THREE.Clock();
     const animate = function () {
       requestAnimationFrame(animate);
+
+      const deltaTime = clock.getDelta();
+
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
+
+      // loop through mixer list
+      for (let i = 0; i < mixerList.length; i++) {
+        const mixer = mixerList[i];
+
+        mixer.update(deltaTime);
+      }
+
+      // loop through vrm list
+      for (let i = 0; i < vrmList.length; i++) {
+        const vrm = vrmList[i];
+
+        vrm.update(deltaTime);
+      }
+
       controls.update();
       renderer.render(scene, camera);
     };
 
     animate();
 
-    const vrmList = [];
-
     const helperRoot = new THREE.Group();
     helperRoot.renderOrder = 10000;
     scene.add(helperRoot);
 
-    helperRoot.visible = true;
+    helperRoot.visible = false;
 
     const defaultModelUrl = 'https://w3s.link/ipfs/QmUmn19HHPVEdREmz46K8YToChNCh3eHb9XWJUT5PLoAsL/default_103.vrm';
 
@@ -121,13 +172,27 @@ const Scene = () => {
       );
     }
 
-    loadUniqueVrm(defaultModelUrl, function (vrm) {
-      const idleAnimation = '/animations/Great Sword Idle.fbx'
-      // const idleAnimation = '/animations/Great Sword Slash.fbx'
-      // const idleAnimation = '/animations/Draw A Great Sword.fbx'
-      // const idleAnimation = '/animations/Great Sword Crouching.fbx'
+    function loadFBXForVRM(animationUrl: string, vrm: any) {
+      // currentAnimationUrl = animationUrl;
 
-      // loadFBX(idleAnimation)
+      // create AnimationMixer for VRM
+      const mixer = new THREE.AnimationMixer(vrm.scene);
+
+      mixerList.push(mixer);
+
+      // Load animation
+      loadMixamoAnimation(animationUrl, vrm).then((clip) => {
+        // Apply the loaded animation to mixer and play
+        mixer.clipAction(clip).play();
+        // mixer.timeScale = params.timeScale;
+        mixer.timeScale = 1.0;
+      });
+    }
+
+    loadUniqueVrm(defaultModelUrl, function (vrm) {
+      const idleAnimation = '/animations/Standard Idle.fbx';
+
+      loadFBXForVRM(idleAnimation, vrm);
 
       vrm.scene.traverse((child: THREE.Object3D) => {
         const name = child.name.trim(); // Exported bones don't have dots in their names
